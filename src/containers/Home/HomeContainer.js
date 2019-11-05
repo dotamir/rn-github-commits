8cc2746c492035700c5391ee9196043840cfebce
Amir <eslami.amir76@gmail.com>
2019-11-05 21:46:53 +0330

    feat(native-base): add native-base ui lib

--- src/containers/Home/HomeContainer.js
+++ src/containers/Home/HomeContainer.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-import {Actions} from 'react-native-router-flux';
-// import {MessageBarManager} from 'react-native-message-bar';
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: 'transparent',
-	},
-});
+import {View, Text} from 'react-native';
+import {Container, Header, Left, Title, Button, Body, Right} from 'native-base';
 
 const HomeContainer = props => {
 	return (
-		<View {...props} style={styles.container}>
-			<Text>Welcome</Text>
-			<Button
-				title="Display Error Modal"
-				onPress={() =>
-					Actions.error({
-						title: 'Error title',
-						data: 'Error explation',
-					})
-				}
-			/>
+		<View {...props}>
+			<Container>
+				<Header>
+					<Left />
+					<Body>
+						<Title>Home</Title>
+					</Body>
+					<Right />
+				</Header>
+			</Container>
 		</View>
 	);
 };
